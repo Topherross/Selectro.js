6a83881be119abbe9d23f4652c61b5816ebe3a54
Christopher Ross <christopherross@Christophers-iMac.local>
2014-08-13 20:26:24 -0700

    Most of the JS is complete.

--- js/selectro.js
+++ js/selectro.js
@@ -1,9 +1,58 @@
 (function () {
     'use strict';
-    var selectro = {};
+    var selectro = {},
+
+        _setAttributes = function(el, attrs){
+            for(var attr in attrs){
+                el.setAttribute(attr, attrs[attr]);
+            }
+
+            return false;
+        },
+
+        _toggleOptions = function(el){
+            var options = el.querySelector('.selectro-options'),
+                display = options.style.display;
+
+            options.style.display = (display === "block")? "none" : "block" ;
+            options.addEventListener('click', function(e){e.stopPropagation();});
+
+            return false;
+        },
+
+        _hideOptions = function(el){
+            var options = el.querySelector('.selectro-options');
+            if(options.style.display === "block") options.style.display = "none";
+
+            return false;
+        };
 
     selectro.init = function(){
-        console.log("Selectro AWAY!!!");
+        var selects = document.querySelectorAll('select.selectro');
+
+        [].forEach.call(selects, function(obj){
+            var new_select = document.createElement('div'),
+                new_options = document.createElement('div');
+
+            _setAttributes(new_select, {'style':'overflow:visible;position:relative;', 'class':'selectro'});
+            _setAttributes(new_options, {'style':'position:relative;display:none;', 'class':'selectro-options'});
+
+            [].forEach.call(obj.querySelectorAll('option'), function(el){
+                var new_a = document.createElement('div'),
+                    attrs = {'style':'display:block;position:relative;', 'data-value':el.value};
+
+                _setAttributes(new_a, attrs);
+
+                new_a.innerText = el.firstChild.nodeValue;
+                new_options.appendChild(new_a);
+            });
+
+            document.body.appendChild(new_select);
+            new_select.appendChild(new_options);
+
+            new_select.addEventListener('click', function(e){e.stopPropagation(); _toggleOptions(this);});
+            document.addEventListener('click', function(){_hideOptions(new_select);});
+        });
     };
 
     if (typeof module !== 'undefined' && module.exports) {
@@ -12,3 +61,21 @@
         window.selectro = selectro;
     }
 })();
+
+if(document.readyState === "complete"){
+    selectro.init();
+}else{
+    if(document.addEventListener){
+        try{
+            document.addEventListener("DOMContentLoaded", selectro.init, false);
+        }catch(e){
+            window.addEventListener("load", selectro.init, false);
+        }
+    }else if(document.attachEvent){
+        try{
+            document.attachEvent("onreadystatechange", selectro.init);
+        }catch(e){
+            window.attachEvent("onload", selectro.init);
+        }
+    }
+}
