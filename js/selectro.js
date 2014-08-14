8955fba0f3869c532b1bdd8e7736ecc1ef7259bf
Christopher Ross <christopherross@Christophers-iMac.local>
2014-08-13 21:40:24 -0700

    Adding dynamic layout vars (really rough).

--- js/selectro.js
+++ js/selectro.js
@@ -35,20 +35,28 @@
                 new_options = document.createElement('div');
 
             _setAttributes(new_select, {'style':'overflow:visible;position:relative;', 'class':'selectro'});
-            _setAttributes(new_options, {'style':'position:relative;display:none;', 'class':'selectro-options'});
+            _setAttributes(new_options, {'style':'position:absolute;display:none;', 'class':'selectro-options'});
 
             [].forEach.call(obj.querySelectorAll('option'), function(el){
-                var new_a = document.createElement('div'),
-                    attrs = {'style':'display:block;position:relative;', 'data-value':el.value};
+                if(el === obj.firstElementChild){
+                    new_select.innerText = el.firstChild.nodeValue;
+                }else {
+                    var new_a = document.createElement('div'),
+                        attrs = {'style': 'display:block;position:relative;', 'data-value': el.value};
 
-                _setAttributes(new_a, attrs);
+                    _setAttributes(new_a, attrs);
 
-                new_a.innerText = el.firstChild.nodeValue;
-                new_options.appendChild(new_a);
+                    new_a.innerText = el.firstChild.nodeValue;
+                    new_options.appendChild(new_a);
+                }
             });
 
             document.body.appendChild(new_select);
             new_select.appendChild(new_options);
+
+            new_select.style.width = new_select.offsetWidth - parseFloat(window.getComputedStyle(new_select, null).getPropertyValue('padding-left')) - parseFloat(window.getComputedStyle(new_select, null).getPropertyValue('padding-right')) -  (parseFloat(window.getComputedStyle(new_select, null).getPropertyValue('border-width'))*2) + "px";
+            new_select.style.height = new_select.offsetHeight - parseFloat(window.getComputedStyle(new_select, null).getPropertyValue('padding-top')) - parseFloat(window.getComputedStyle(new_select, null).getPropertyValue('padding-bottom')) -  (parseFloat(window.getComputedStyle(new_select, null).getPropertyValue('border-width'))*2) + "px";
+            new_options.style.width = parseFloat(new_select.style.width) + parseFloat(window.getComputedStyle(new_select, null).getPropertyValue('padding-left')) + parseFloat(window.getComputedStyle(new_select, null).getPropertyValue('padding-right')) +  (parseFloat(window.getComputedStyle(new_select, null).getPropertyValue('border-width'))*2) - (parseFloat(window.getComputedStyle(new_options, null).getPropertyValue('border-width'))*2) + "px";
 
             new_select.addEventListener('click', function(e){e.stopPropagation(); _toggleOptions(this);});
             document.addEventListener('click', function(){_hideOptions(new_select);});
