87fd90da0fb090f8e07b3e3c1242d272caa90e67
Christopher Ross <cross@phluant.com>
2014-08-14 21:30:44 -0700

    Added arrow, and began work on search input.

--- js/selectro.js
+++ js/selectro.js
@@ -2,6 +2,8 @@
     'use strict';
     var selectro = {},
 
+        _values = "undefined",
+
         _setAttributes = function(el, attrs){
             for(var attr in attrs){
                 el.setAttribute(attr, attrs[attr]);
@@ -23,7 +25,10 @@
 
         _hideOptions = function(el){
             var options = el.querySelector('.selectro-options');
-            if(options.style.display === "block") options.style.display = "none";
+            if(options.style.display === "block"){
+                options.style.display = "none";
+                el.classList.toggle('selected');
+            }
 
             return false;
         };
@@ -33,19 +38,29 @@
 
         [].forEach.call(selects, function(obj){
             var new_select = document.createElement('div'),
+                arrow = document.createElement('span'),
+                search = document.createElement('input'),
                 new_options = document.createElement('div');
+
+            _values = (typeof _values === "undefined")? [] : _values;
 
             _setAttributes(new_select, {'style':'overflow:visible;position:relative;', 'class':'selectro'});
             _setAttributes(new_options, {'style':'position:absolute;display:none;', 'class':'selectro-options'});
+            _setAttributes(arrow, {'style':'display:inline-block;position:relative;vertical-align:middle;border-color:rgb(140,140,140) transparent transparent transparent;border-width:7px 5px 0 5px;border-style:solid;width:0;height:0;margin:0 0 0 8px;'});
+            _setAttributes(search, {'type':'text', 'class':'selectro-search', 'style':'display:block;border:rgb(96,96,96) 1px solid;'});
+
+            new_options.appendChild(search);
 
             [].forEach.call(obj.querySelectorAll('option'), function(el){
                 if(el === obj.firstElementChild){
                     new_select.innerText = el.firstChild.nodeValue;
                 }else {
                     var new_a = document.createElement('div'),
-                        attrs = {'style': 'display:block;position:relative;', 'data-value': el.value};
+                        attributes = {'style': 'display:block;position:relative;', 'data-value': el.value};
 
-                    _setAttributes(new_a, attrs);
+                    _setAttributes(new_a, attributes);
+
+                    if(typeof _values === "array" && _values.indexOf(el.value) === -1)  _values.push(el.value);
 
                     new_a.innerText = el.firstChild.nodeValue;
                     new_options.appendChild(new_a);
@@ -53,6 +68,7 @@
             });
 
             document.body.appendChild(new_select);
+            new_select.appendChild(arrow);
             new_select.appendChild(new_options);
 
             new_select.style.width = new_select.offsetWidth - parseFloat(window.getComputedStyle(new_select, null).getPropertyValue('padding-left')) - parseFloat(window.getComputedStyle(new_select, null).getPropertyValue('padding-right')) -  (parseFloat(window.getComputedStyle(new_select, null).getPropertyValue('border-width'))*2) + "px";
@@ -63,6 +79,14 @@
             new_select.addEventListener('click', function(e){e.stopPropagation(); _toggleOptions(this);});
             document.addEventListener('click', function(){_hideOptions(new_select);});
         });
+    };
+
+    selectro.getParent = function(){
+        return _parent;
+    };
+
+    selectro.getValue = function(){
+        return _replacement.value;
     };
 
     if (typeof module !== 'undefined' && module.exports) {
