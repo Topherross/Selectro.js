a0b2a531a9098125cdaf0cf03669f74f191ba8dc
Christopher Ross <cross@phluant.com>
2014-08-19 22:27:27 -0700

    Additional private functions added for calculating width, and height. Added wrapper element, and search listener.

--- js/selectro.js
+++ js/selectro.js
@@ -12,8 +12,34 @@
             return false;
         },
 
+        _calculateWidth = function(el, append_px){
+            var styles = window.getComputedStyle(el, null),
+                calculated_width = el.offsetWidth -
+                parseFloat(styles.getPropertyValue('padding-left')) -
+                parseFloat(styles.getPropertyValue('padding-right')) -
+                (parseFloat(styles.getPropertyValue('border-left-width')) + parseFloat(styles.getPropertyValue('border-right-width')));
+
+            if(typeof append_px === "boolean" && append_px === true)
+                calculated_width += "px";
+
+            return calculated_width;
+        },
+
+        _calculateHeight = function(el, append_px){
+            var styles = window.getComputedStyle(el, null),
+                calculated_height = el.offsetHeight -
+                parseFloat(styles.getPropertyValue('padding-top')) -
+                parseFloat(styles.getPropertyValue('padding-bottom')) -
+                (parseFloat(styles.getPropertyValue('border-top-width')) + parseFloat(styles.getPropertyValue('border-bottom-width')));
+
+            if(typeof append_px === "boolean" && append_px === true)
+                calculated_height += "px";
+
+            return calculated_height;
+        },
+
         _toggleOptions = function(el){
-            var options = el.querySelector('.selectro-options'),
+            var options = el.parentNode.querySelector('.selectro-options'),
                 display = options.style.display;
 
             options.style.display = (display === "block")? "none" : "block" ;
@@ -24,32 +50,39 @@
         },
 
         _hideOptions = function(el){
-            var options = el.querySelector('.selectro-options');
+            var options = el.parentNode.querySelector('.selectro-options');
             if(options.style.display === "block"){
                 options.style.display = "none";
                 el.classList.toggle('selected');
             }
 
             return false;
+        },
+
+        _search = function(search){
+            window.addEventListener('keyup', function(){
+                console.log(search.value);
+            });
         };
 
     selectro.init = function(){
         var selects = document.querySelectorAll('select.selectro');
 
+        _values = (typeof _values === "undefined")? [] : _values;
+
         [].forEach.call(selects, function(obj){
-            var new_select = document.createElement('div'),
+            var select_wrap = document.createElement('div'),
+                new_select = document.createElement('div'),
                 arrow = document.createElement('span'),
                 serach_wrap = document.createElement('div'),
                 glass = document.createElement('span'),
                 search = document.createElement('input'),
                 new_options = document.createElement('div');
 
-            _values = (typeof _values === "undefined")? [] : _values;
-
             _setAttributes(new_select, {'style':'overflow:visible;position:relative;', 'class':'selectro'});
-            _setAttributes(new_options, {'style':'position:absolute;display:none;', 'class':'selectro-options'});
+            _setAttributes(new_options, {'style':'position:relative;display:none;', 'class':'selectro-options'});
             _setAttributes(arrow, {'style':'display:inline-block;position:relative;vertical-align:middle;border-color:rgb(140,140,140) transparent transparent transparent;border-width:7px 5px 0 5px;border-style:solid;width:0;height:0;margin:0 0 0 8px;'});
-            _setAttributes(search, {'type':'text', 'class':'selectro-search', 'style':'display:block;border:rgb(96,96,96) 1px solid;'});
+            _setAttributes(search, {'type':'text', 'class':'selectro-search', 'style':'display:block;', 'name':'selectro-search'});
 
             new_options.appendChild(search);
 
@@ -69,17 +102,20 @@
                 }
             });
 
-            document.body.appendChild(new_select);
+            obj.parentNode.appendChild(select_wrap);
+            select_wrap.appendChild(new_select);
             new_select.appendChild(arrow);
-            new_select.appendChild(new_options);
+            select_wrap.appendChild(new_options);
 
-            new_select.style.width = new_select.offsetWidth - parseFloat(window.getComputedStyle(new_select, null).getPropertyValue('padding-left')) - parseFloat(window.getComputedStyle(new_select, null).getPropertyValue('padding-right')) -  (parseFloat(window.getComputedStyle(new_select, null).getPropertyValue('border-width'))*2) + "px";
-            new_select.style.height = new_select.offsetHeight - parseFloat(window.getComputedStyle(new_select, null).getPropertyValue('padding-top')) - parseFloat(window.getComputedStyle(new_select, null).getPropertyValue('padding-bottom')) -  (parseFloat(window.getComputedStyle(new_select, null).getPropertyValue('border-width'))*2) + "px";
-            new_options.style.width = parseFloat(new_select.style.width) + parseFloat(window.getComputedStyle(new_select, null).getPropertyValue('padding-left')) + parseFloat(window.getComputedStyle(new_select, null).getPropertyValue('padding-right')) +  (parseFloat(window.getComputedStyle(new_select, null).getPropertyValue('border-width'))*2) - (parseFloat(window.getComputedStyle(new_options, null).getPropertyValue('border-width'))*2) + "px";
-            new_options.style.top = parseFloat(new_select.style.height) + parseFloat(window.getComputedStyle(new_select, null).getPropertyValue('padding-top')) + parseFloat(window.getComputedStyle(new_select, null).getPropertyValue('padding-bottom')) + (parseFloat(window.getComputedStyle(new_select, null).getPropertyValue('border-width'))) + "px";
+            new_select.style.width =  _calculateWidth(new_select, true);
+            select_wrap.style.width = new_select.offsetWidth + "px";
+            new_select.style.height = _calculateHeight(new_select, true);
 
-            new_select.addEventListener('click', function(e){e.stopPropagation(); _toggleOptions(this);});
+            new_select.addEventListener('click', function(e){e.stopPropagation(); _toggleOptions(new_select);});
+            search.addEventListener('focus', function(){_search(search);});
             document.addEventListener('click', function(){_hideOptions(new_select);});
+
+            obj.style.display = "none";
         });
     };
 
