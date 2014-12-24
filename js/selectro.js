93b962c96cc2c700f24861273eb3e4ff58c4b3e2
Christopher Ross <cross@phluant.com>
2014-12-24 11:38:54 -0800

    Added grid selector Selectro type, and started to implement the search functionality.

--- js/selectro.js
+++ js/selectro.js
@@ -65,19 +65,21 @@
 
         _toggleOptions = function(index){
             for(var i in objects){
-                if(objects.hasOwnProperty(i)){
+                if(objects.hasOwnProperty(i) && typeof objects[i].options_wrap !== "undefined"){
                     if(index === parseFloat(i)){
-                        objects[i].new_options.style.display = (objects[i].new_options.style.display === "block")? "none" : "block" ;
-                        objects[i].new_options.style.zIndex = (objects[i].new_options.style.display === "block")? "10000" : "auto" ;
+                        objects[i].options_wrap.style.display = (objects[i].options_wrap.style.display === "block")? "none" : "block" ;
+                        objects[i].options_wrap.style.zIndex = (objects[i].options_wrap.style.display === "block")? "10000" : "auto" ;
                         objects[i].new_select.classList.toggle('selected');
-                        objects[i].new_options.addEventListener('click', function(e){e.stopPropagation();});
+                        objects[i].options_wrap.addEventListener('click', function(e){e.stopPropagation();});
 
-                        if(objects[i].new_options.style.display === "block")
+                        if(objects[i].options_wrap.style.display === "block"){
+                            objects[i].search.value = "";
                             objects[i].search.focus();
+                        }
 
-                    }else if(objects[i].new_options.style.display === "block"){
-                        objects[i].new_options.style.display = "none";
-                        objects[i].new_options.style.zIndex = "auto";
+                    }else if(objects[i].options_wrap.style.display === "block"){
+                        objects[i].options_wrap.style.display = "none";
+                        objects[i].options_wrap.style.zIndex = "auto";
                         objects[i].new_select.classList.toggle('selected');
                     }
                 }
@@ -88,9 +90,11 @@
 
         _hideOptions = function(){
             for(var i in objects){
-                if(objects.hasOwnProperty(i) && objects[i].new_options.style.display === "block"){
-                    objects[i].new_options.style.display = "none";
-                    objects[i].new_options.style.zIndex = "auto";
+                if(objects.hasOwnProperty(i) &&
+                    typeof objects[i].options_wrap !== "undefined" &&
+                    objects[i].options_wrap.style.display === "block"){
+                    objects[i].options_wrap.style.display = "none";
+                    objects[i].options_wrap.style.zIndex = "auto";
                     objects[i].new_select.classList.toggle('selected');
                 }
             }
@@ -123,7 +127,29 @@
                 _toggleOptions(i);
 
                 if(typeof _configs.afterSelect === "function")
-                    _configs.afterSelect(objects[i]);
+                    _configs.afterSelect(objects[i], el);
+            }
+
+            return false;
+        },
+
+        _selectTile = function(el, i){
+            if(el.hasAttribute("data-disabled") && el.getAttribute("data-disabled") === "disabled")
+                return false;
+
+            if(objects.hasOwnProperty(i)){
+                objects[i].original_input.value = el.getAttribute("data-value");console.log(objects[i].original_input.value);
+
+                for(var tile in objects[i].tiles){
+                    if(objects[i].tiles.hasOwnProperty(tile) && objects[i].tiles[tile].classList.contains('selected')){
+                        objects[i].tiles[tile].classList.remove('selected');
+                    }
+                }
+
+                el.classList.add('selected');
+
+                if(typeof _configs.afterSelect === "function")
+                    _configs.afterSelect(objects[i], el);
             }
 
             return false;
@@ -139,10 +165,212 @@
             return false;
         },
 
-        _search = function(search){
-            window.addEventListener('keyup', function(){
-                if(typeof console !== "undefined")
-                    console.log(search.value);
+        _search = function(el, i){
+            if(el.value === "")
+                return;
+
+            if(typeof console !== "undefined")
+                console.log(el.value, i);
+        },
+
+        _buildMobile = function(objs){
+            [].forEach.call(objs, function(el){
+                el.addEventListener("change", function(){
+                    if(_configs.links)
+                        window.location.assign(el.value);
+                    else if(typeof _configs.afterSelect === "function")
+                        _configs.afterSelect(el);
+                });
+            });
+
+            if(typeof _configs.afterInit === "function")
+                _configs.afterInit(objects);
+        },
+
+        _buildLists = function(objs){
+            [].forEach.call(objs, function(obj, index){
+                var _objs = {
+                        original_input : obj,
+                        select_wrap : _createEl('div', {'class':'selectro-wrap', 'style':'position:relative;'}),
+                        new_select : _createEl('div', {'style':'overflow:visible;position:relative;', 'class':obj.classList, 'id':(obj.hasAttribute('id'))? 'selectro_'+obj.getAttribute('id') : 'selectro_'+index}),
+                        label : _createEl('span', {'class':'selectro-label default'}),
+                        arrow : _createEl('span', {'class':'selectro-arrow', 'style':'display:inline-block;position:relative;vertical-align:middle;border-color:rgb(140,140,140) transparent transparent transparent;border-width:7px 5px 0 5px;border-style:solid;width:0;height:0;'}),
+                        options_wrap : _createEl('div', {'style':'position:absolute;display:none;', 'class':'selectro-options-wrap'}),
+                        new_options : _createEl('div', {'class':'selectro-options'})
+                    },
+                    object_count = objects.length,
+                    label = "Select an Option",
+                    select_children = obj.children,
+                    sibling = obj.nextElementSibling;
+
+                if(_configs.searchable === true) {
+                    _objs.search_wrap = _createEl('div', {'class':'selectro-search-wrap', 'style':'overflow:auto;'});
+                    _objs.search = _createEl('input', {'class':'selectro-search', 'type':'text'});
+                    _objs.options_wrap.appendChild(_objs.search_wrap);
+
+                    if (_configs.searchIcon === true) {
+                        _objs.search_icon = _createEl('div', {'class':'selectro-search-icon', 'style':'float:right;'});
+                        _objs.search_wrap.appendChild(_objs.search_icon);
+                    }
+
+                    _objs.search_wrap.appendChild(_objs.search);
+
+                    _objs.search.addEventListener('keyup', function(){
+                        _search(this, object_count);
+                    }, false);
+                    _objs.search.addEventListener('paste', function(){
+                        // @TODO: Timeout used until clipboardData is accessible in a consistent cross-browser environment. 12-24-2014
+                        setTimeout(function(){
+                            _search(_objs.search, object_count);
+                        }, 20);
+                    }, false);
+                }
+
+                [].forEach.call(select_children, function(el){
+                    var _create_obj = function(obj){
+                        if(obj === select_children[0] && !el.hasAttribute('value')){
+                            label = el.value;
+                            _objs.original_input.value = "";
+                            return;
+                        }
+
+                        var new_a = _createEl('div', {'class':'selectro-option', 'style': 'display:block;position:relative;', 'data-value': obj.value});
+
+                        _setText(new_a, obj.firstChild.nodeValue);
+                        _objs.new_options.appendChild(new_a);
+
+                        if(_configs.links)
+                            new_a.addEventListener("click", function(){_link(this);});
+                        else
+                            new_a.addEventListener("click", function(){_select(this, object_count);});
+
+                        if(obj.hasAttribute('selected')){
+                            new_a.classList.add('selected');
+                            _setText(_objs.label, _getText(obj));
+                            _objs.label.classList.remove('default');
+                        }
+                    };
+
+                    if(el.tagName.toLowerCase() === "optgroup"){
+                        if(typeof el.getAttribute("label") !== "undefined"){
+                            var opt_header = _createEl("h6", {'class':'selectro-optgroup-header'});
+
+                            _setText(opt_header, el.getAttribute("label"));
+                            _objs.new_options.appendChild(opt_header);
+                        }
+
+                        [].forEach.call(el.children, function(child){
+                            _create_obj(child);
+                        });
+                    }else{
+                        _create_obj(el);
+                    }
+                });
+
+                _setText(_objs.label, label);
+
+                if(typeof sibling !== "undefined")
+                    obj.parentNode.insertBefore(_objs.select_wrap, sibling);
+                else
+                    obj.parentNode.appendChild(_objs.select_wrap);
+
+                _objs.select_wrap.appendChild(_objs.new_select);
+                _objs.select_wrap.appendChild(_objs.options_wrap);
+                _objs.options_wrap.appendChild(_objs.new_options);
+
+                _objs.new_select.appendChild(_objs.label);
+                _objs.new_select.appendChild(_objs.arrow);
+
+                _objs.new_select.addEventListener('click', function(e){
+                    e.stopPropagation();
+                    _toggleOptions(object_count);
+                });
+                _objs.search.addEventListener('focus', function(){
+                    _search(_objs.search);
+                });
+                document.addEventListener('click', function(){
+                    _hideOptions();
+                });
+
+                objects.push(_objs);
+
+                obj.style.display = "none";
+            });
+        },
+
+        _buildGrids = function(objs){
+            [].forEach.call(objs, function(obj, index){
+                var _objs = {
+                        original_input : obj,
+                        grid_wrap : _createEl('div', {'class':'selectro-grid-wrap', 'style':'position:relative;'}),
+                        grid : _createEl('div', {'class':'selectro-grid'}),
+                        tiles : []
+                    },
+                    object_count = objects.length,
+                    label = "Select an Option",
+                    select_children = obj.children,
+                    sibling = obj.nextElementSibling;
+
+                [].forEach.call(select_children, function(el){
+                    var _create_obj = function(obj){
+                        if(obj === select_children[0] && !el.hasAttribute('value')){
+                            label = el.value;
+                            _objs.original_input.value = "";
+                            return;
+                        }
+
+                        var new_option = _createEl('div', {'class':'selectro-grid-option', 'style': 'display:block;position:relative;', 'data-value': obj.value}),
+                            tile = _createEl('div', {'class':'selectro-tile'}),
+                            tile_inner = _createEl('div', {'class':'selectro-tile-inner'}),
+                            tile_label = _createEl('div', {'class':'selectro-grid-option-label'});
+
+                        _setText(tile_label, obj.firstChild.nodeValue);
+                        _objs.grid.appendChild(new_option);
+                        new_option.appendChild(tile_label);
+                        new_option.appendChild(tile);
+                        tile.appendChild(tile_inner);
+
+                        new_option.addEventListener("click", function(){
+                            _selectTile(this, object_count);
+                        });
+
+                        if(obj.hasAttribute('selected') && !obj.hasAttribute('disabled')){
+                            new_option.classList.add('selected');
+                        }else if(obj.hasAttribute('disabled')){
+                            _setAttributes(new_option, {'data-disabled':'disabled'});
+                            new_option.classList.add('disabled');
+                        }
+
+                        _objs.tiles.push(new_option);
+                    };
+
+                    if(el.tagName.toLowerCase() === "optgroup"){
+                        if(typeof el.getAttribute("label") !== "undefined"){
+                            var opt_header = _createEl("h6", {'class':'selectro-grid-optgroup-header'});
+
+                            _objs.grid.appendChild(opt_header);
+                        }
+
+                        [].forEach.call(el.children, function(child){
+                            _create_obj(child);
+                        });
+                    }else{
+                        _create_obj(el);
+                    }
+                });
+
+                // _setText(_objs.label, label);
+
+                if(typeof sibling !== "undefined")
+                    obj.parentNode.insertBefore(_objs.grid_wrap, sibling);
+                else
+                    obj.parentNode.appendChild(_objs.grid_wrap);
+
+                _objs.grid_wrap.appendChild(_objs.grid);
+
+                objects.push(_objs);
+
+                obj.style.display = "none";
             });
         };
 
@@ -154,110 +382,20 @@
         if(typeof _configs.beforeInit === "function")
             _configs.beforeInit();
 
-        var selects = document.querySelectorAll(".selectro");
+        var selects = document.querySelectorAll(".selectro"),
+            grids = document.querySelectorAll(".selectro-grid");
 
         if(_browser() === "mobile"){
-            if(_configs.links){
-                [].forEach.call(selects, function(el){
-                    el.addEventListener("change", function(){window.location.assign(el.value);});
-                });
-            }
+            _buildMobile(selects);
+
             return;
         }
 
-        [].forEach.call(selects, function(obj, index){
-            var _objs = {
-                    original_input : obj,
-                    select_wrap : _createEl('div', {'class':'selectro-wrap', 'style':'position:relative;'}),
-                    new_select : _createEl('div', {'style':'overflow:visible;position:relative;', 'class':obj.classList, 'id':(obj.hasAttribute('id'))? 'selectro_'+obj.getAttribute('id') : 'selectro_'+index}),
-                    label : _createEl('span', {'class':'selectro-label default'}),
-                    arrow : _createEl('span', {'class':'selectro-arrow', 'style':'display:inline-block;position:relative;vertical-align:middle;border-color:rgb(140,140,140) transparent transparent transparent;border-width:7px 5px 0 5px;border-style:solid;width:0;height:0;'}),
-                    new_options : _createEl('div', {'style':'position:absolute;display:none;', 'class':'selectro-options'})
-                },
-                object_count = objects.length,
-                label = "Select an Option",
-                select_children = obj.children,
-                sibling = obj.nextElementSibling;
-
-            if(_configs.searchable === true) {
-                _objs.search_wrap = _createEl('div', {'class':'selectro-search-wrap', 'style':'overflow:auto;'});
-                _objs.search = _createEl('input', {'class':'selectro-search', 'type':'text'});
-                _objs.new_options.appendChild(_objs.search_wrap);
-
-                if (_configs.searchIcon === true) {
-                    _objs.search_icon = _createEl('div', {'class':'selectro-search-icon', 'style':'float:right;'});
-                    _objs.search_wrap.appendChild(_objs.search_icon);
-                }
-
-                _objs.search_wrap.appendChild(_objs.search);
-            }
-
-            [].forEach.call(select_children, function(el){
-                var _create_obj = function(obj){
-                    if(obj === select_children[0] && !el.hasAttribute('value')){
-                        label = el.value;
-                        _objs.original_input.value = "";
-                        return;
-                    }
-
-                    var new_a = _createEl('div', {'class':'selectro-option', 'style': 'display:block;position:relative;', 'data-value': obj.value});
-
-                    _setText(new_a, obj.firstChild.nodeValue);
-                    _objs.new_options.appendChild(new_a);
-
-                    if(_configs.links)
-                        new_a.addEventListener("click", function(){_link(this);});
-                    else
-                        new_a.addEventListener("click", function(){_select(this, object_count);});
-
-                    if(obj.hasAttribute('selected')){
-                        new_a.classList.add('selected');
-                        _setText(_objs.label, _getText(obj));
-                        _objs.label.classList.remove('default');
-                    }
-                };
-
-                if(el.tagName.toLowerCase() === "optgroup"){
-                    if(typeof el.getAttribute("label") !== "undefined"){
-                        var opt_header = _createEl("h6", {'class':'selectro-optgroup-header'});
-
-                        _setText(opt_header, el.getAttribute("label"));
-                        _objs.new_options.appendChild(opt_header);
-                    }
-
-                    [].forEach.call(el.children, function(child){
-                        _create_obj(child);
-                    });
-                }else{
-                    _create_obj(el);
-                }
-            });
-
-            _setText(_objs.label, label);
-
-            if(typeof sibling !== "undefined")
-                obj.parentNode.insertBefore(_objs.select_wrap, sibling);
-            else
-                obj.parentNode.appendChild(_objs.select_wrap);
-
-            _objs.select_wrap.appendChild(_objs.new_select);
-            _objs.select_wrap.appendChild(_objs.new_options);
-
-            _objs.new_select.appendChild(_objs.label);
-            _objs.new_select.appendChild(_objs.arrow);
-
-            _objs.new_select.addEventListener('click', function(e){e.stopPropagation(); _toggleOptions(object_count);});
-            _objs.search.addEventListener('focus', function(){_search(_objs.search);});
-            document.addEventListener('click', function(){_hideOptions();});
-
-            objects.push(_objs);
-
-            obj.style.display = "none";
-        });
+        _buildLists(selects);
+        _buildGrids(grids);
 
         if(typeof _configs.afterInit === "function")
             _configs.afterInit(objects);
-
     };
 
     if (typeof module !== 'undefined' && module.exports) {
